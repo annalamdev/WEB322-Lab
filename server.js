b091ec719ed4ed5688e2a36757da936d65ef11c5
Anna Lam <annalam@macbookair.home>
2026-02-23 00:00:53 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Lok Man Lam WEB322 " + new Date().toISOString());
+});
+
+app.listen(3000, () => console.log("Server running on port 3000"));
