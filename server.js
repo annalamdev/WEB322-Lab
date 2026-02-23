43543553a483459d76f0cb378e38c9e783684c40
Anna Lam <annalam@macbookair.home>
2026-02-23 00:07:26 -0500

    updated text

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Lok Man Lam WEB322 " + new Date().toISOString());
+  res.send("Lok Man Lam - WEB322 " + new Date().toISOString());
 });
 
 app.listen(3000, () => console.log("Server running on port 3000"));
